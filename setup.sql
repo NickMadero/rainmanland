cf31d26a8621a2c91c892a6b436d70572769f632
NickMadero <madero22@students.rowan.edu>
2023-02-28 20:56:02 -0500

    BIG use of frontend backend docker and nginx all working together.

---
+++ setup.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `user_test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) NOT NULL,
+  `password_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
