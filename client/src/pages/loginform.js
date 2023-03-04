d719f8f9dd54dd9ebcf09f35a157a536650bd97f
NickMadero <madero22@students.rowan.edu>
2023-03-04 11:53:36 -0500

    in login the create account button will now take you to the signup page. still need to figure out how to get pass login to the customer info page

--- client/src/pages/loginform.js
+++ client/src/pages/loginform.js
@@ -1,82 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './login-form.css';
-import axios from "axios";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-class LoginForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            setUserName: '',
-            setPassword: ''
-        };
-    }
+function LoginForm() {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-    handleChange = (Event) => {
-        let name = Event.target.name;
-        let password = Event.target.value;
-        this.setState ({
-            [name]:password
-        })
-    };
-
-
-    submit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        axios.post('/api/insert', this.state)
+        axios
+            .post('/api/insert', { username, password })
             .then(() => {
                 alert('success post');
-                this.props.navigate('/rainmanland/client/src/pages/customerinfo.js');
+                navigate('/rainmanland/client/src/pages/customerinfo.js');
             })
             .catch((error) => {
                 console.log(error);
             });
-    }
-
-    handleCreateAccount = () => {
-        this.props.navigate('/rainmanland/src/pages/signupPage.js');
     };
 
-    render() {
-        return (
-            <div>
-                <div className="login-form-login-form">
-                    <form className="login-form-form" onSubmit={this.submit}>
-                        <span className="login-form-text">login</span>
-                        <span className="login-form-text1">email</span>
-                        <input
-                            name="setUserName"
-                            required
-                            placeholder="enter email"
-                            className="login-form-textinput input"
-                            onChange={this.handleChange}
-                        />
-                        <span className="login-form-text2">
-                            <span>password</span>
-                            <br></br>
-                        </span>
-                        <input
-                            name="setPassword"
-                            type="password"
-                            required
-                            placeholder="enter password"
-                            className="login-form-textinput1 input"
-                            onChange={this.handleChange}
-                        />
-                        <button className="login-form-button button">forgot password?</button>
-                        <button type="submit" className="login-form-button1 button">
-                            login
-                        </button>
-                        <button
-                            className="login-form-link"
-                            onClick={this.handleCreateAccount}
-                        >
-                            create account
-                        </button>
-                    </form>
-                </div>
+    const handleCreateAccount = () => {
+        navigate('/rainmanland/client/src/pages/signupPage.js');
+    };
+
+    return (
+        <div>
+            <div className="login-form-login-form">
+                <form className="login-form-form" onSubmit={handleSubmit}>
+                    <span className="login-form-text">login</span>
+                    <span className="login-form-text1">email</span>
+                    <input
+                        name="username"
+                        required
+                        placeholder="enter email"
+                        className="login-form-textinput input"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <span className="login-form-text2">
+            <span>password</span>
+            <br />
+          </span>
+                    <input
+                        name="password"
+                        type="password"
+                        required
+                        placeholder="enter password"
+                        className="login-form-textinput1 input"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button className="login-form-button button">forgot password?</button>
+                    <button type="submit" className="login-form-button1 button">
+                        login
+                    </button>
+                    <button className="login-form-link" onClick={handleCreateAccount}>
+                        create account
+                    </button>
+                </form>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default LoginForm;
+
