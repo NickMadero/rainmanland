f0dc7943f3832bd136b39c891e6c82e2b1e4db82
NickMadero <madero22@students.rowan.edu>
2023-03-08 15:23:36 -0500

    added some new fields to customer info

--- client/src/components/CustomerInfoPage.jsx
+++ client/src/components/CustomerInfoPage.jsx
@@ -28,22 +28,36 @@ const CustomerInfoPage = (props) => {
             </Row>
             <Row>
                 <Col>
-                    <Form.Group controlId="formUnitsPerZone">
-                        <Form.Label>How many units per zone?</Form.Label>
-                        <Form.Control type="number" placeholder="# of units" required min="0" />
-                    </Form.Group>
-                </Col>
-                <Col>
                     <Form.Group controlId="formNumZones">
                         <Form.Label>How many zones?</Form.Label>
                         <Form.Control type="number" placeholder="# of zones" required min="0" />
                     </Form.Group>
                 </Col>
+                <Col>
+                    <Form.Group controlId="emailAddress">
+                        <Form.Label>What is your email address?</Form.Label>
+                        <Form.Control type="number" placeholder="email Address" required min="0" />
+                    </Form.Group>
+                </Col>
             </Row>
             <Form.Group controlId="formAddress">
                 <Form.Label>What is your address?</Form.Label>
-                <Form.Control type="text" placeholder="enter address" required />
+                <Form.Control type="text" placeholder="enter Address" required />
             </Form.Group>
+            <Row>
+                <Col>
+                    <Form.Group controlId="FirstName">
+                        <Form.Label>Enter First Name</Form.Label>
+                        <Form.Control type="text" placeholder="First Name" required pattern="[A-Za-z ]+" />
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group controlId="LastName">
+                        <Form.Label>Enter Last Name</Form.Label>
+                        <Form.Control type="text" placeholder="Last Name" required pattern="[A-Za-z ]+" />
+                    </Form.Group>
+                </Col>
+            </Row>
             <Button  onClick={props.onGoToCalendarButtonClick} variant="primary" type="submit" style={{ margin: '30px', color: 'white', backgroundColor: 'blue' }}>
                 Book a new appointment
             </Button>
