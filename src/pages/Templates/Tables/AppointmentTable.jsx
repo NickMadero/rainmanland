e15a4a19e315dccdd38ae1394b1efc0006381a4b
sauce-picante <tdougherty16@gmail.com>
2023-03-18 17:55:08 -0400

    overhaul to comply with React framework

---
+++ src/pages/Templates/Tables/AppointmentTable.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import styles from './AppointmentTable.module.css';
+import Button from '../../Templates/Button/Button';
+
+const SelectableAppointmentTable = () =>{
+    return(
+        <div id="schedule-table" className={styles['schedule-table']}>
+            <AppointmentTable appointments={APPOINTMENTS}  />
+            <Button variant={'remove'} text={'Remove Appointment'} type={"submit"} />
+        </div>
+    ); 
+}
+
+function AppointmentTable({ appointments={APPOINTMENTS}  }){
+    const rows = [];
+
+    appointments.forEach((appointment) => {
+        
+    rows.push(
+        <AppointmentRow
+        appointment={appointment}
+        key={appointment.name} />
+    );
+    });
+
+    return (
+    <table
+        id='table--appointments'
+        name='table--appointments'
+        className={styles['table--appointments']}
+        >
+        <thead>
+            <tr>
+            <th>
+                <h4>AM/PM</h4>
+            </th>
+            <th>
+                <h4>Name</h4>
+            </th>
+            <th>
+                <h4>Phone</h4>
+            </th>
+            <th>
+                <h4>Address</h4>
+            </th>
+            <th>
+                <h4>ZIP/Service Area</h4>
+            </th>
+            </tr>
+        </thead>
+            <tbody id='table-body--appointments' className={styles['table-body--apointments']}>{rows}</tbody>
+        </table>
+    );
+    }
+    
+   
+    
+    function AppointmentRow({ appointment }){
+        return (
+        <tr>
+            <td>{appointment.time}</td>
+            <td>{appointment.name}</td>
+            <td>{appointment.phone}</td>
+            <td>{appointment.address}</td>
+            <td>{appointment.zip}</td>
+        </tr>
+        );
+    }
+    
+    const APPOINTMENTS = [
+        {time: "AM", name: "RZA", phone: "(917)812-4547", address: "6701 Amboy Rd, Staten Island, NY", zip: "10312"},
+        {time: "AM", name: "GZA", phone: "(917)327-2254", address: "212 Lamberts Ln, Staten Island, NY", zip: "10304"},
+        {time: "PM", name: "Ghostface Killah", phone: "(646)355-7986", address: "201 Demorest Ave, Staten Island, NY", zip: "10304"},
+        {time: "PM", name: "Method Man", phone: "(347)996-3606", address: "195 Steuben St Apt 4L Staten Island, NY", zip: "10306"},
+        {time: "PM", name: "Raekwon", phone: "(347)378-6671", address: "22A Julie Ct, Staten Island, NY", zip: "10306"},
+    ];
+export default SelectableAppointmentTable;
--- src/pages/ownerhomepage.js
+++ src/pages/ownerhomepage.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import styles from './ownerhomepage.module.css'
+import styles from './Templates/ownerhomepage.module.css'
+import SelectableAppointmentTable from './Templates/Tables/AppointmentTable.jsx'
 /**
  *  TO-DO: Entry for lists of zipcodes for service areas
  *         Service area map (Leaflet? Google Maps?)
@@ -10,231 +11,20 @@ import styles from './ownerhomepage.module.css'
 const OwnerHomePage = (props) => {
   /*EVENT LISTENERS & HANDLERS GO HERE */
   return (
-    <div className={styles['page']}>
+    <>
     <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    </head>
-      
-      
-      <h1 className={styles['text']}>
-        <span>Business Settings</span>
-      </h1>
-      {/** Container for Service Area and Controller Pricing settings */}
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      </head>
+    <div className={styles['page']}>
       <div id="container--top" className={styles['top-container']}>
-        {/**Form for sprinkler controller pricing info */}
-        <form
-          id='form--controllers'
-          name='form--controllers'
-          className={styles['form--controllers']}
-          >
-            <h3 id="heading--controllers" className={styles['heading--controllers']}>
-              <span className={styles['heading--controllers']}>Controller Pricing</span>
-            </h3>
-            <div
-              id='container-controller-table'
-              name='container-controller-table'
-              className={styles['container-controller-table']}>
-            <section
-              id='table--controllers'
-              name='table--controllers'
-              className={styles['table--controlllers']}
-              >
-                <thead>
-                  <tr>
-                    <th>
-                      <h4>Brand</h4>
-                    </th>
-                    <th>
-                      <h4>Cost Multiplier</h4>
-                    </th>
-                  </tr>
-                </thead>
-                 {/** TO DO: Generate table with JavaScript function **/}
-                <tbody id='table-body--controllers' className={styles['table-body--controllers']}>
-                  <tr>
-                      <td>test brand 1</td>
-                      <td>1.5x</td>
-                  </tr>
-                  <tr>
-                      <td>test brand 2</td>
-                      <td>1.5x</td>
-                  </tr>
-                  <tr>
-                      <td>test brand 3</td>
-                      <td>1.5x</td>
-                  </tr>
-                  <tr>
-                      <td>test brand 4</td>
-                      <td>1.5x</td>
-                  </tr>
-                  <tr>
-                      <td>test brand 5</td>
-                      <td>1.5x</td>
-                  </tr>
-                </tbody>
-              </section>
-              </div>
-              <input
-                id='input--brand'
-                name='input--brand'
-                className={styles['input--brand']}
-                placeholder='Controller Brand Name'
-                >
-              </input>
-              <input
-                id='input--price'
-                name='input--price'
-                className={styles['input--price']}
-                placeholder='Price'
-                >
-                </input>
-          </form>
-        {/*BEGIN SERVICE AREA SETTINGS*/}
-        <form
-          id="form--service-area"
-          name="form"
-          className={styles['form--service-area']}
-        >
-          <div className={styles['service-area-wrapper']}>
-            <h3 id="heading--srvc-area" className={styles['heading-srvc-area']}>
-              <span className={styles['heading--srvc-area']}>Business Address & Service Area</span>
-            </h3>
-            <form
-              id="input--business-address"
-              name="business-street-address"
-              className={styles['address-form']}
-            >
-              <input
-                type="text"
-                id="input--business-address1"
-                placeholder="Business Address 1"
-                className={styles['biz-address1']}
-              />
-              <input
-                type="text"
-                id="input--business-address2"
-                placeholder="Business Address 2"
-                className={styles['biz-address2']}
-              />
-              <input
-                type="text"
-                id="input--city"
-                name="input city"
-                placeholder="City"
-                className={styles['input-city']}
-              />
-              <input
-                type="text"
-                id="input--state"
-                name="input-state"
-                placeholder="State"
-                className={styles['input-state']}
-              />
-              <input
-                type="text"
-                id="input--zip-code"
-                name="zip code"
-                value="ZIP"
-                placeholder="ZIP"
-                className={styles['input-zip']}
-              />
-              <h4 className={styles['radius']}>
-                <span>Radius:</span>
-              </h4>
-              <select
-                id="slct--radius"
-                name="select-radius"
-                className={styles['select-radius']}
-              >
-                <option value="5">5 miles</option>
-                <option value="10">10 miles</option>
-                <option value="15">15 miles</option>
-                <option value="20">20 miles</option>
-                <option value="25">25 miles</option>
-          </select>
-            <button
-            id="btn--srvc-area"
-            name="btn--srvc-area"
-            className={styles['confirm']}
-          >
-            <span id="btn-confirm-text" className={styles['btn-confirm-text']}>
-              <span>Confirm</span>
-            </span>
-            </button>
-            </form>
-           </div>
-            
-          </form>
-        {/** END SERVICE AREA SETTINGS */}
+
       </div>
-      {/** Container for time tracking */}
-      <div id="container--bottom" className={styles['bottom-container']}>
-        <div id="container--schedule" className={styles['container--schedule']}>
-          <h3 id='heading-schedule' className ={styles['heading-schedule']}>Schedule</h3>
-            <section 
-              id='table--appointments'
-              name='table--appointments'
-              className={styles['table--appointments']}
-              >
-                <thead>
-                  <tr>
-                    <th>
-                      <h4>AM/PM</h4>
-                    </th>
-                    <th>
-                      <h4>Name</h4>
-                    </th>
-                    <th>
-                      <h4>Address</h4>
-                    </th>
-                    <th>
-                      <h4>ZIP/Service Area</h4>
-                    </th>
-                    <th>
-                      <h4>Phone</h4>
-                    </th>
-                  </tr>
-                </thead>
-                {/**TO DO: Generate table with JavaScript function **/}
-                  <tbody id='table-body--appointments' className={styles['table-body--apointments']}>
-                      <tr>
-                        <td>AM</td>
-                        <td>Bo Jackson</td>
-                        <td>356 Haddon Ave Collingswood, NJ</td>
-                        <td>08107</td>
-                        <td>(856)555-7161</td>
-                      </tr>
-                      <tr>
-                        <td>AM</td>
-                        <td>Bo Jackson</td>
-                        <td>356 Haddon Ave Collingswood, NJ</td>
-                        <td>08107</td>
-                        <td>(856)555-7161</td>
-                      </tr>
-                      <tr>
-                        <td>AM</td>
-                        <td>Bo Jackson</td>
-                        <td>356 Haddon Ave Collingswood, NJ</td>
-                        <td>08107</td>
-                        <td>(856)555-7161</td>
-                      </tr>
-                      <tr>
-                        <td>AM</td>
-                        <td>Bo Jackson</td>
-                        <td>356 Haddon Ave Collingswood, NJ</td>
-                        <td>08107</td>
-                        <td>(856)555-7161</td>
-                      </tr>
-                  </tbody>
-              </section>
-              <button
-              id='btn--get-schedule'
-              name='btn--get-schedule'
-              className={``}></button>
-        </div>
+    <div id="container--bottom" className={styles['bottom-container']}>
+        <SelectableAppointmentTable />
       </div>
     </div>
-  )
+    </>
+  );
 }
 
 export default OwnerHomePage
