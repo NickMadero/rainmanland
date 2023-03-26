8c08eeca06d47758edac4686362a7eed07595b81
Nicholas Madero <105759449+NickMadero@users.noreply.github.com>
2023-03-25 22:34:10 -0400

    Update AvailabilitySettings.jsx

--- nginx/client/src/components/Dashboard-Parts/Availability/AvailabilitySettings.jsx
+++ nginx/client/src/components/Dashboard-Parts/Availability/AvailabilitySettings.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import styles from './AvailabilitySettings.module.css';
-import DateTimePicker from '../../Components/DateTimePicker/DateTimePicker.jsx'
+import styles from '../Availabilty/AvailabliltySetting.module.css';
+import DateTimePicker from '../Components/DateTimePicker/DateTimePicker'
 const AvailabilitySettings = () => {
 
     return(
         <div id='wrapper' name='availability-wrapper' className={styles.wrapper}>
             <h2 id='heading' className={styles.heading}>Availability</h2>
             <DateTimePicker text={'date input 1'}/>
-            <DateTimePicker text={'date input 2'}/>
+
         </div>
     );
 }
