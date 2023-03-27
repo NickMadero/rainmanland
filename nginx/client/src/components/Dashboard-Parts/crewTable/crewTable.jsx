c2bb32cfaac9390e5b0835c2ab17b07ac03488b0
NickMadero <madero22@students.rowan.edu>
2023-03-27 13:13:00 -0400

    added crew list to ownerdashboard shows list of crews. when clicking on crew name it will show all crew members in the crew

---
+++ nginx/client/src/components/Dashboard-Parts/crewTable/crewTable.jsx
@@ -0,0 +1,75 @@
+import axios from 'axios';
+import { Table, Form, Button, FormGroup, FormLabel, Modal } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+
+function CrewTable() {
+    const [crews, setCrews] = useState([]);
+    const [selectedCrew, setSelectedCrew] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        // Fetch crew data from backend API
+        axios.post('/api/get-crew', { name: 'one' })
+            .then(response => {
+                console.log(response.data);
+                setCrews(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    const handleCrewClick = (crew) => {
+        setSelectedCrew(crew);
+        setShowModal(true);
+    }
+
+    return (
+        <div style={{ position: "absolute", top: 15, left: 0, width: '50%', height: '50%', overflowY: 'scroll' }}>
+            <FormGroup>
+                <Form.Label>Crew List</Form.Label>
+            </FormGroup>
+            <Table striped bordered >
+                <thead>
+                <tr>
+                    <th>Crews</th>
+                </tr>
+                </thead>
+                <tbody>
+                {crews.map(crew => (
+                    <tr key={crew.name}>
+                        <td>
+                            <Button variant="link" onClick={() => handleCrewClick(crew)}>
+                                {crew.name}
+                            </Button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </Table>
+
+            <Modal show={showModal} onHide={() => setShowModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Crew Members for {selectedCrew?.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <ul>
+                        {selectedCrew?.members.map(member => (
+                            <li key={member.id} className="row">
+                                <div className="col" style={{ whiteSpace: 'nowrap' }}>{member.first_name} {member.last_name} {member.emailaddress}</div>
+                                <div className="col-auto"><Form.Check>
+                                    <Form.Check.Input type="checkbox" />
+                                    <Form.Check.Label style={{ fontSize: '12px' }}>Remove</Form.Check.Label>
+                                </Form.Check></div>
+                            </li>
+
+
+                        ))}
+                    </ul>
+                </Modal.Body>
+            </Modal>
+        </div>
+    );
+}
+
+export default CrewTable;
--- nginx/client/src/components/OwnerDashboard.jsx
+++ nginx/client/src/components/OwnerDashboard.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import styles from '../style/ownerdashboard.module.css'
 import SelectableAppointmentTable from './Dashboard-Parts/AppointmentTable/AppointmentTable.jsx'
+import crewTable from "./Dashboard-Parts/crewTable/crewTable";
 import AvailabilitySettings from '../components/Dashboard-Parts/Availabilty/AvailablilitySetting'
+import CrewTable from "./Dashboard-Parts/crewTable/crewTable";
 /**
  *  TO-DO: Entry for lists of zipcodes for service areas
  *         Service area map (Leaflet? Google Maps?)
@@ -16,7 +18,7 @@ const OwnerHomePage = (props) => {
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
             <div className={styles['page']}>
-
+                <CrewTable />
                 <div id="container--bottom" className={styles['bottom-container']}>
                     <SelectableAppointmentTable />
                 </div>
