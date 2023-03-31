b8234ca652692f11eb14567473bde9c44ab7f322
Marcus Tangradi <mtangradi5@gmail.com>
2023-03-31 16:01:28 -0400

    working get all appointments, crew name, settings for generating the available half Days

--- api/calander/generateHalfDay.js
+++ api/calander/generateHalfDay.js
@@ -8,5 +8,22 @@
  *  Takes a crew, zip code, time slots, first/second half, if its available (used for weekends)
  *  and stores the generated half days in the database
  *
- *  
+ *
  */
+
+/**
+ * this function will
+ */
+function generateHalfDaysForCrew(crew, zipCodes, settings, callback){
+
+    console.log(zipCodes);
+    console.log(crew);
+    console.log(settings);
+
+    callback(callback);
+}
+
+
+
+
+module.exports = {generateHalfDaysForCrew};
