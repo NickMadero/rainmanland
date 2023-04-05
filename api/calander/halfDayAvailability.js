d4b57c32b4cd3a4d6d643e1606378bdfed53c316
Marcus Tangradi <mtangradi5@gmail.com>
2023-04-05 11:54:10 -0400

    added checkDistance for appointment, need to implement logic

--- api/calander/halfDayAvailability.js
+++ api/calander/halfDayAvailability.js
@@ -12,10 +12,11 @@
 
 async function checkCalendarAvailability(calendar, appointment){
 
+    console.log(appointment);
 
     for(let i = 0; i < calendar.halfDays.length; i++ ){
-        calendar.halfDays[i] = await checkHalfDay(calendar.halfDays[i], appointment);
-        // console.log(calendar.halfDays[i]);
+        calendar.halfDays[i] = await checkHalfDay(calendar.halfDays[i], appointment, calendar.crewName);
+        console.log(calendar.halfDays[i]);
     }
 
 
@@ -24,19 +25,44 @@ async function checkCalendarAvailability(calendar, appointment){
 
 /**
  * this will check all half days and set if they are available or not based on the appointment
+ * all if statements will only change the isAvailable variable to false
  * @param halfDay
  * @param appointment
  * @returns {Promise<void>}
  */
-async function checkHalfDay(halfDay, appointment){
+async function checkHalfDay(halfDay, appointment, crewName){
 
+    //check if an appointent is too far from an existing appointment on a half day
+    if( await checkDistanceBetweenAppointmentsTooFar(halfDay, appointment, crewName) === true){
+        halfDay.isAvailable = 0;
+    }
 
     // console.log(halfDay);
-    halfDay.isAvailable = halfDay.isAvailable = 0;
-
+    // halfDay.isAvailable = halfDay.isAvailable = 0;
+    // halfDay.isAvailable = 0;
 
     return Promise.resolve(halfDay);
 }
+
+/**
+ * THIS WILL use the google maps api to check the distance between an existing appointment on a half day and the
+ * appointment trying to be scheduled
+ *
+ * @param halfDay a single half day from the calendar
+ * @param appointment the new appointment that is trying to be scheduled
+ * @returns {Promise<boolean>} a boolean value to determine if an appointment is too far
+ */
+async function checkDistanceBetweenAppointmentsTooFar(halfDay, appointment, crewName){
+    let isTooFar = false;
+
+    //this will query the database to get all the appointments on a half day to compare distance to new appointment
+    let storedHalfDay = await getStoredHalfDay(halfDay, crewName);
+
+
+
+    return Promise.resolve(isTooFar);
+}
+
 
 
 module.exports = {checkCalendarAvailability};
